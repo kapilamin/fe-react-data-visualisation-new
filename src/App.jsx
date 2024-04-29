ef77706e51b5cfc714e87727b90912d00e18bd03
HannahLDugan <155827170+HannahLDugan@users.noreply.github.com>
2024-04-29 12:31:57 +0100

    started adding in useEffect functionality

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,19 @@ import './App.css'
 import Header from './components/Header'
 import TVSearch from './components/TVSearch'
 import TVList from './components/TVList'
+import { useState } from 'react'
 
 function App() {
+
+  const [currentSearchTerm, setCurrentSearchTerm] = useState("")
 
   
 
   return (
     <>
       <Header />
-      <TVSearch />
-      <TVList />
+      <TVSearch setCurrentSearchTerm={ setCurrentSearchTerm }/>
+      <TVList currentSearchTerm= { currentSearchTerm }/>
     </>
   )
 }
--- src/components/TVList.jsx
+++ src/components/TVList.jsx
@@ -1,5 +1,36 @@
-function TVList (){
+import { useState } from "react";
+import { useEffect } from "react";
 
-}
+function TVList (props){
+    const { currentSearchTerm } = props;
+
+    const [TvShows, setTvShows] = useState([])
+
+    useEffect(() => {
+        fetch(`https://api.tvmaze.com/search/shows?q=${currentSearchTerm}`).then((response) => {
+            return response.json()
+    }).then((data) => {
+   //    console.log(data)
+            setTvShows(data)
+        })
+    }, [currentSearchTerm])
+
+    return (<> 
+    <h2>{`Here are all of our tv shows on ${currentSearchTerm}`} </h2>
+        <ul>
+            {TvShows.map((TvShow) => {
+                return (
+                <li key={TvShow.id} className="tvshow">
+                    console.log("")
+                    <h2>{TvShow.show.name}</h2>
+                    <h2>{TvShow.show.genre}</h2>
+                    <img src={TvShow.show.image.medium}/>
+                 </li>
+                )
+            })}
+        </ul>
+    </>)
+
+    }
 
 export default TVList
