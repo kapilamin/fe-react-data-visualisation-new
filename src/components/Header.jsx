0bb62d1965c700cb5c96ae5b9628fbb1bd45390a
HannahLDugan <155827170+HannahLDugan@users.noreply.github.com>
2024-04-30 15:43:19 +0100

    incorporated components from bootstrap

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,16 @@
+import Badge from 'react-bootstrap/Badge';
+
 function Header (){
     return(
-    <h1>📺 Telly Addicts 🍿 </h1>
-    )
+//     <h1 className="Header">📺 Telly Addicts 🍿 </h1>
+//     <h6> Example heading <Badge bg="secondary">New</Badge>
+//   </h6>
+<div>
+<h1 className="Header">📺 Telly Addicts 🍿 </h1>
+<h2> <Badge bg="secondary">Welcome to Telly Addicts</Badge>
+</h2>
+</div>
+);
 }
 
 export default Header
--- src/components/TVList.jsx
+++ src/components/TVList.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import ListGroup from 'react-bootstrap/ListGroup';
 
 function TVList ({ currentSearchTerm, currentRating}){
 
@@ -29,18 +30,7 @@ function TVList ({ currentSearchTerm, currentRating}){
     }
     return (<> 
 
-            {/* <div>
-                <h2>{`Here are all of our tv shows on ${currentSearchTerm}`}</h2>
-                <label htmlFor="ratingFilter">Filter by Rating:</label>
-                <input
-                    type="number"
-                    id="ratingFilter"
-                    value={ratingFilter || ""}
-                    onChange={setRatingFilter}
-                />
-            </div> */}
-
-    <h2>{`Here are all of our tv shows on ${currentSearchTerm}`} </h2>
+    <h2 className="subHeading">{`Here are all of our tv shows on ${currentSearchTerm}`} </h2>
         <ul>
         {TvShows.map((TvShow) => {
         const { show } = TvShow;
@@ -49,24 +39,19 @@ function TVList ({ currentSearchTerm, currentRating}){
         const genreList = genres.join(', ');
         const showRating = rating?.average ?? 'No rating'; 
 
-        // if (ratingFilter && parseFloat(showRating) !== ratingFilter) {
-        //     return null;
-        // }
-
-
         return (
             <li key={TvShow.id} className="tvshow">
-                <h2 className="show">{name}</h2>
-                <h2 className="genres">{genreList}</h2>
-                <h2 className="ratings">{showRating}</h2>
+                 <ListGroup variant="flush">
+               <ListGroup.Item><h2 className="show">{name}</h2></ListGroup.Item>
+               <ListGroup.Item><h2 className="genres">{genreList}</h2></ListGroup.Item>
+              <ListGroup.Item><h2 className="ratings">{showRating}</h2></ListGroup.Item>
+              </ListGroup>
                 {image ? (
                     <img src={image.medium} alt={`Poster for ${name}`} />
                 ) : (
                     <div>No Image Available</div>
                 )}
-            </li>
-
-            
+            </li>      
         );
     })}
         </ul>
@@ -74,4 +59,7 @@ function TVList ({ currentSearchTerm, currentRating}){
 
     }
 
-export default TVList
+export default TVList;
+
+
+// export default FlushExample;
